5e0197c0a8cc0032c94a6fc00642fd6f64c4c37c
FrostBite <51112725+vivek-cyberange@users.noreply.github.com>
2020-03-17 05:32:03 +0000

    Mod

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-PARAMS="--permit-write"
+PARAMS="--permit-write --reconnect --reconnect-time 1"
 
 if [[ -n $BASIC_AUTH_USER || -n $BASIC_AUTH_PASS ]]; then
         PARAMS="$PARAMS --credential $BASIC_AUTH_USER:$BASIC_AUTH_PASS"
