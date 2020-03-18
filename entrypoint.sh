62c04ff353844fac3df742911bdb2a36f3736d2a
FrostBite <51112725+vivek-cyberange@users.noreply.github.com>
2020-03-18 09:55:42 +0000

    Add pcap link

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,5 @@ if [[ -n $BASIC_AUTH_USER || -n $BASIC_AUTH_PASS ]]; then
         PARAMS="$PARAMS --credential $BASIC_AUTH_USER:$BASIC_AUTH_PASS"
 fi
 
+wget https://github.com/cyberharsh/hack/blob/master/WEPcrack-01.cap?raw=true
 gotty $PARAMS /bin/bash
